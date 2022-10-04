28eed377a784fb15afd6bca4e604ed621222be58
Ahmad Jajja <86593662+Ahmadjajja@users.noreply.github.com>
2022-10-04 21:26:11 +0500

    Typescript chapter 5 reading completed from tutorial point Book

--- Typescript/Typescript practices/Chapter_1_to_5.js
+++ Typescript/Typescript practices/Chapter_1_to_5.js
@@ -3,3 +3,28 @@
 var str = '12345';
 var str2 = str; //str is now of type number
 console.log(str2);
+//infered typing
+// In infered typing, TypeScript will find the first usage of the 
+// variable within the code, determine the type to which it has been initially set and then 
+// assume the same type for this variable in the rest of your code block.
+// var num=2; // data type inferred as number
+// console.log("value of num "+num);
+// num="12";
+// console.log(num);
+//Above code throws an error because initially variable set to Number and now assigning string value to this number
+//Example: Variable Scope
+var global_num = 12; //global variable
+var Numbers = /** @class */ (function () {
+    function Numbers() {
+        this.num_val = 13; //class variable
+    }
+    Numbers.prototype.storeNum = function () {
+        var local_num = 14; //local variable
+    };
+    Numbers.sval = 10; //static field
+    return Numbers;
+}());
+console.log("Global num: " + global_num);
+console.log(Numbers.sval); //static variable 
+var obj = new Numbers();
+console.log("Global num: " + obj.num_val);
--- Typescript/Typescript practices/Chapter_1_to_5.ts
+++ Typescript/Typescript practices/Chapter_1_to_5.ts
@@ -2,6 +2,52 @@
 
 //Type assertion 
 
-var str='12345'
-var str2:number=<number> <any> str //str is now of type number
+
+var str = '12345'
+var str2: number = <number><any>str //str is now of type number
 console.log(str2);
+
+//infered typing
+// In infered typing, TypeScript will find the first usage of the 
+// variable within the code, determine the type to which it has been initially set and then 
+// assume the same type for this variable in the rest of your code block.
+
+
+
+// var num=2; // data type inferred as number
+// console.log("value of num "+num);
+// num="12";
+// console.log(num);
+
+
+
+//Above code throws an error because initially variable set to Number and now assigning string value to this number
+
+
+//Example: Variable Scope
+var global_num = 12 //global variable
+class Numbers {
+    num_val = 13; //class variable
+    static sval = 10; //static field
+    storeNum(): void {
+        var local_num = 14; //local variable
+    }
+}
+console.log("Global num: " + global_num)
+console.log(Numbers.sval) //static variable 
+var obj = new Numbers();
+console.log("Global num: " + obj.num_val)
+
+
+
+
+
+
+
+
+
+
+
+
+
+

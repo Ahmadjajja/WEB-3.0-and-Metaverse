4629cba136eadb01a6af81781cc4b445825e38aa
Ahmad Jajja <86593662+Ahmadjajja@users.noreply.github.com>
2022-10-04 18:55:27 +0500

    Typescript installation guide added in Typescript Repo

---
+++ Typescript/Typescript practices/Chapter_1_to_5.js
@@ -0,0 +1,5 @@
+//Chapter 5 Practices
+//Type assertion 
+var str = '12345';
+var str2 = str; //str is now of type number
+console.log(str2);
---
+++ Typescript/Typescript practices/Chapter_1_to_5.ts
@@ -0,0 +1,7 @@
+//Chapter 5 Practices
+
+//Type assertion 
+
+var str='12345'
+var str2:number=<number> <any> str //str is now of type number
+console.log(str2);

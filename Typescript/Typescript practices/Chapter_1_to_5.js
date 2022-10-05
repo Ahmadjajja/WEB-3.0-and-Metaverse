2c201571250b07303e42738f4d847027cb43055a
Ahmad Jajja <86593662+Ahmadjajja@users.noreply.github.com>
2022-10-05 07:37:33 +0500

    Typescript Chapter 6 reading and Practice Completed from tutorial point book

--- Typescript/Typescript practices/Chapter_1_to_5.js
+++ Typescript/Typescript practices/Chapter_1_to_5.js
@@ -3,7 +3,7 @@
 var str = '12345';
 var str2 = str; //str is now of type number
 console.log(str2);
-//infered typing
+// infered typing
 // In infered typing, TypeScript will find the first usage of the 
 // variable within the code, determine the type to which it has been initially set and then 
 // assume the same type for this variable in the rest of your code block.
@@ -13,18 +13,24 @@ console.log(str2);
 // console.log(num);
 //Above code throws an error because initially variable set to Number and now assigning string value to this number
 //Example: Variable Scope
-var global_num = 12; //global variable
-var Numbers = /** @class */ (function () {
-    function Numbers() {
-        this.num_val = 13; //class variable
-    }
-    Numbers.prototype.storeNum = function () {
-        var local_num = 14; //local variable
-    };
-    Numbers.sval = 10; //static field
-    return Numbers;
-}());
-console.log("Global num: " + global_num);
-console.log(Numbers.sval); //static variable 
-var obj = new Numbers();
-console.log("Global num: " + obj.num_val);
+// Little Confusion in classes concept
+// var global_num = 12 //global variable
+// class Numbers {
+//     num_val = 13; //class variable
+//     static sval = 10; //static field
+//     storeNum(): void {
+//         var local_num = 14; //local variable
+//     }
+// }
+// console.log("Global num: " + global_num)
+// console.log(Numbers.sval) //static variable 
+// var obj = new Numbers();
+// console.log("Global num: " + obj.num_val)
+// // classes concept
+// class Greeting {
+//     greet(): void {
+//         console.log("Hello World!!!")
+//     }
+// }
+// var obj2 = new Greeting();
+// obj2.greet();
--- Typescript/Typescript practices/Chapter_1_to_5.ts
+++ Typescript/Typescript practices/Chapter_1_to_5.ts
@@ -1,13 +1,13 @@
 //Chapter 5 Practices
 
-//Type assertion 
+                                                    //Type assertion 
 
 
 var str = '12345'
 var str2: number = <number><any>str //str is now of type number
 console.log(str2);
 
-//infered typing
+                                                    // infered typing
 // In infered typing, TypeScript will find the first usage of the 
 // variable within the code, determine the type to which it has been initially set and then 
 // assume the same type for this variable in the rest of your code block.
@@ -24,19 +24,31 @@ console.log(str2);
 //Above code throws an error because initially variable set to Number and now assigning string value to this number
 
 
-//Example: Variable Scope
-var global_num = 12 //global variable
-class Numbers {
-    num_val = 13; //class variable
-    static sval = 10; //static field
-    storeNum(): void {
-        var local_num = 14; //local variable
-    }
-}
-console.log("Global num: " + global_num)
-console.log(Numbers.sval) //static variable 
-var obj = new Numbers();
-console.log("Global num: " + obj.num_val)
+                                                    //Example: Variable Scope
+                                                    // Little Confusion in classes concept
+// var global_num = 12 //global variable
+// class Numbers {
+//     num_val = 13; //class variable
+//     static sval = 10; //static field
+//     storeNum(): void {
+//         var local_num = 14; //local variable
+//     }
+// }
+// console.log("Global num: " + global_num)
+// console.log(Numbers.sval) //static variable 
+// var obj = new Numbers();
+// console.log("Global num: " + obj.num_val)
+
+
+// // classes concept
+// class Greeting {
+//     greet(): void {
+//         console.log("Hello World!!!")
+//     }
+// }
+// var obj2 = new Greeting();
+// obj2.greet();
+
 
 
 

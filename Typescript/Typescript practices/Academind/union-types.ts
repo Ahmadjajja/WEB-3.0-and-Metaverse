0c12a5f1548b3e87f44b603a6cfff756734948ed
Ahmad Jajja <86593662+Ahmadjajja@users.noreply.github.com>
2022-10-07 23:41:11 +0500

    In,Typescript going throw...

--- Typescript/Typescript practices/Academind/union-types.ts
+++ Typescript/Typescript practices/Academind/union-types.ts
@@ -28,27 +28,59 @@
 
 //type aliases
 
-type combinable = 'as-string' | 'as-number'  //type aliases
+// type conversionDescriptor = 'as-string' | 'as-number'  //type aliases
 
-function combine(input1:number | string, input2: number | string, resultConversion: combinable) {
-    let result;
-    if (typeof input1 === 'number' && typeof input2 === 'number' && resultConversion === 'as-number') {
-        result = +input1 + +input2;
-    } else {
-        result = input1.toString() + input2.toString();
-    }
+// function combine(input1:number | string, input2: number | string, resultConversion: conversionDescriptor) {
+//     let result;
+//     if (typeof input1 === 'number' && typeof input2 === 'number' && resultConversion === 'as-number') {
+//         result = +input1 + +input2;
+//     } else {
+//         result = input1.toString() + input2.toString();
+//     }
 
-    return result
+//     return result
 
-    // if (resultConversion === 'as-number') {
-    //     return +result;
-    // } else {
-    //     return result.toString();
-    // }
+//     // if (resultConversion === 'as-number') {
+//     //     return +result;
+//     // } else {
+//     //     return result.toString();
+//     // }
     
+// }
+// console.log(combine(4,29, "as-number"));
+// console.log(combine(4,29, "as-string"));
+
+
+
+
+// Never type
+
+function generateError(message: string, code: number) {
+    throw  { message: message, errorCode: code};
 }
-console.log(combine(4,29, "as-number"));
-console.log(combine(4,29, "as-string"));
+
+
+
+generateError("An error occurred!", 500) 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 

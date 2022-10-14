129875b59f325b83f72de1b603c7557017a0109c
Ahmad Jajja <86593662+Ahmadjajja@users.noreply.github.com>
2022-10-14 11:53:19 +0500

    Typescript book 17 chapter read...

---
+++ Typescript/Typescript practices/classes.js
@@ -0,0 +1,9 @@
+var AgriLoan = /** @class */ (function () {
+    function AgriLoan(interest, rebate) {
+        this.interest = interest;
+        this.rebate = rebate;
+    }
+    return AgriLoan;
+}());
+var obj = new AgriLoan(10, 1);
+console.log("Interest is : " + obj.interest + " Rebate is : " + obj.rebate);

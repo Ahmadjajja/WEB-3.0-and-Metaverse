7736f1de3d413542b8dd8d4d66805812e60400d5
Ahmad Jajja <86593662+Ahmadjajja@users.noreply.github.com>
2022-11-05 08:18:57 +0500

    JS book chapter 13 project

---
+++ WEB 2.0/Javascript/concurrency chapter 13/project1.mjs
@@ -0,0 +1,20 @@
+let allowedPasswords = ["ahmad786","sultan786","jajja786","programmer786","softwareEngineer786","ceo786","trainer786"]; //stored allowed passwords
+
+let login = (password) => allowedPasswords.includes(password);      //uses for checking password is in allowedPassword_array or not.
+
+let passwordValidity = (password) => {
+    return new Promise ((resolve, reject) => {   //step 3
+        if (login(password) === true) {
+            resolve(true);
+        } else {
+            reject(false);
+        }
+    })
+}
+passwordValidity("ahmad78").then((value) => {    //step 4 & step 5
+    console.log(value)
+})
+.catch((value) => {
+    console.log(value)
+})
+

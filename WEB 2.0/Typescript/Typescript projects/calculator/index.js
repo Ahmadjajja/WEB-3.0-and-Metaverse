47b3979f8403fe8b2c38b8da52c30988da09fe0d
Ahmad Jajja <86593662+Ahmadjajja@users.noreply.github.com>
2022-11-28 11:11:34 +0500

    updated

--- WEB 2.0/Typescript/Typescript projects/calculator/index.js
+++ WEB 2.0/Typescript/Typescript projects/calculator/index.js
@@ -1,61 +1,76 @@
-"use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
-var __generator = (this && this.__generator) || function (thisArg, body) {
-    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
-    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
-    function verb(n) { return function (v) { return step([n, v]); }; }
-    function step(op) {
-        if (f) throw new TypeError("Generator is already executing.");
-        while (g && (g = 0, op[0] && (_ = 0)), _) try {
-            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
-            if (y = 0, t) op = [op[0] & 2, t.value];
-            switch (op[0]) {
-                case 0: case 1: t = op; break;
-                case 4: _.label++; return { value: op[1], done: false };
-                case 5: _.label++; y = op[1]; op = [0]; continue;
-                case 7: op = _.ops.pop(); _.trys.pop(); continue;
-                default:
-                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
-                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
-                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
-                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
-                    if (t[2]) _.ops.pop();
-                    _.trys.pop(); continue;
-            }
-            op = body.call(thisArg, _);
-        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
-        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
-    }
-};
-exports.__esModule = true;
-var chalkAnimation = require("chalk-animation");
-var sleep = function () {
-    return new Promise(function (res) {
-        setTimeout(res, 2000);
-    });
-};
-function welcome() {
-    return __awaiter(this, void 0, void 0, function () {
-        var rainbowTitle;
-        return __generator(this, function (_a) {
-            switch (_a.label) {
-                case 0:
-                    rainbowTitle = chalkAnimation.rainbow('Lorem ipsum dolor sit amet');
-                    return [4 /*yield*/, sleep()];
-                case 1:
-                    _a.sent();
-                    rainbowTitle.stop(); //stop  
-                    return [2 /*return*/];
-            }
-        });
-    });
-}
-welcome();
+// import chalkAnimation from 'chalk-animation';
+// const sleep = () => {
+//     return new Promise((res) => {
+//         setTimeout(res, 2000);
+//     });
+// };
+// async function welcome() {
+//     let rainbowTitle = chalkAnimation.rainbow('Lorem ipsum dolor sit amet'); //start
+//     await sleep();
+//     rainbowTitle.stop(); //stop  
+// }
+// welcome();
+
+// // let operator ;
+// // let result ;
+// // inquirer
+// //   .prompt([
+// //     /* Pass your questions in here */
+// //     {
+// //         name: "operator",
+// //         type: "list",
+// //         message : "Enter operator",
+// //         choices: [
+// //             "+",
+// //             "-",
+// //             "*",
+// //             "/"
+// //         ]
+// //     }
+// //   ])
+// //   .then((op) => {
+// //     inquirer
+// //   .prompt([
+// //     /* Pass your questions in here */
+// //     {
+// //         name: "firstNumber",
+// //         type: "number",
+// //         message : "Enter first number => ",
+// //     }
+// //   ])
+// //   .then((num1) => {
+// //     result = num1.firstNumber op.operator  num1.firstNumber
+// //     // Use user feedback for... whatever!!
+// //   })
+// //   .catch((error) => {
+// //     if (error.isTtyError) {
+// //       // Prompt couldn't be rendered in the current environment
+// //     } else {
+// //       // Something else went wrong
+// //     }
+// //   });
+// //     // operator = op.operator;   //+
+// //     // switch (answers.operator) {
+// //     //     case "+":
+// //     //         break;
+// //     //     case "-":
+// //     //         break;
+// //     //     case "/":
+// //     //         break;
+// //     //     case "*":
+// //     //         break;
+// //     //     default:
+// //     //         break;
+// //     // }
+// //     // Use user feedback for... whatever!!
+// //   })
+// //   .catch((error) => {
+// //     if (error.isTtyError) {
+// //       // Prompt couldn't be rendered in the current environment
+// //       console.log(error.message)
+// //     } else {
+// //       // Something else went wrong
+// //       console.log(error)
+// //     }
+// //   });
+
--- WEB 2.0/Typescript/Typescript projects/calculator/index.ts
+++ WEB 2.0/Typescript/Typescript projects/calculator/index.ts
@@ -1,17 +1,221 @@
-import chalk from "chalk";
-import * as chalkAnimation from 'chalk-animation';
+// import chalk from "chalk";
+// import chalkAnimation from 'chalk-animation';
+// import inquirer from "inquirer";
+// // import Choices from "inquirer/lib/objects/choices.js";
 
-const sleep = () => {
-    return new Promise((res) => {
-        setTimeout(res, 2000);
-    })
-}
 
-async function welcome() {
-    let rainbowTitle = chalkAnimation.rainbow('Lorem ipsum dolor sit amet');  //start
+// // const sleep = () => {
+// //     return new Promise((res) => {
+// //         setTimeout(res, 2000);
+// //     })
+// // }
 
-    await sleep();  
-    rainbowTitle.stop();  //stop  
-}
+// // async function welcome() {
+// //     let rainbowTitle = chalkAnimation.rainbow('Lorem ipsum dolor sit amet');  //start
 
-welcome();
+// //     await sleep();  
+// //     rainbowTitle.stop();  //stop  
+// // }
+
+
+
+// // welcome();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // let operator ;
+// // let result ;
+
+// // inquirer
+// //   .prompt([
+// //     /* Pass your questions in here */
+// //     {
+// //         name: "operator",
+// //         type: "list",
+// //         message : "Enter operator",
+// //         choices: [
+// //             "+",
+// //             "-",
+// //             "*",
+// //             "/"
+// //         ]
+
+// //     }
+// //   ])
+// //   .then((op) => {
+    
+
+// //     inquirer
+// //   .prompt([
+// //     /* Pass your questions in here */
+// //     {
+// //         name: "firstNumber",
+// //         type: "number",
+// //         message : "Enter first number => ",
+// //     }
+// //   ])
+// //   .then((num1) => {
+// //     result = num1.firstNumber op.operator  num1.firstNumber
+// //     // Use user feedback for... whatever!!
+
+// //   })
+// //   .catch((error) => {
+// //     if (error.isTtyError) {
+// //       // Prompt couldn't be rendered in the current environment
+// //     } else {
+// //       // Something else went wrong
+// //     }
+// //   });
+
+// //     // operator = op.operator;   //+
+
+// //     // switch (answers.operator) {
+// //     //     case "+":
+            
+// //     //         break;
+// //     //     case "-":
+            
+// //     //         break;
+// //     //     case "/":
+            
+// //     //         break;
+// //     //     case "*":
+            
+// //     //         break;
+    
+// //     //     default:
+// //     //         break;
+// //     // }
+    
+// //     // Use user feedback for... whatever!!
+// //   })
+// //   .catch((error) => {
+// //     if (error.isTtyError) {
+// //       // Prompt couldn't be rendered in the current environment
+// //       console.log(error.message)
+// //     } else {
+// //       // Something else went wrong
+// //       console.log(error)
+// //     }
+// //   });
+
+
+
+
+
+
+
+

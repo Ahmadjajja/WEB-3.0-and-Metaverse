7feff396e053c05e870203199590f40721af17a6
Ahmad Jajja <86593662+Ahmadjajja@users.noreply.github.com>
2022-12-21 08:21:18 +0500

    reading chapter 2 of typescript book by josh goldberg

--- WEB 2.0/Typescript/Typescript Practice from Book/chapter#01/index.js
+++ WEB 2.0/Typescript/Typescript Practice from Book/chapter#01/index.js
@@ -1,1 +1,9 @@
-console.log("Nothing is worth more than laughter.");
+"use strict";
+console.log(Symbol("Franklin"));
+console.log("Ahmad");
+let bestSong = Math.random() > 0.5 ? parseInt("2") : "Respect";
+let firstName = "Whitney";
+firstName.length;
+console.log(typeof (firstName.length));
+let nam = false;
+nam: string = "ahmad";
--- WEB 2.0/Typescript/Typescript Practice from Book/chapter#01/index.ts
+++ WEB 2.0/Typescript/Typescript Practice from Book/chapter#01/index.ts
@@ -1,1 +1,11 @@
-console.blub("Nothing is worth more than laughter.");
+console.log(Symbol("Franklin"));
+console.log("Ahmad");
+let bestSong = Math.random() > 0.5 ? parseInt("2") : "Respect";
+let firstName = "Whitney";
+firstName.length;
+console.log(typeof(firstName.length));
+
+let nam = false
+nam:string = "ahmad"
+
+

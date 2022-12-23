247c5d345a3ccf572bd37caa7848a86ea0347ace
Ahmad Jajja <86593662+Ahmadjajja@users.noreply.github.com>
2022-12-24 00:05:10 +0500

    first 6 chapter reading of typescript book by josh goldberg

--- WEB 2.0/Typescript/Typescript Practice from Book/chapter#01/index.ts
+++ WEB 2.0/Typescript/Typescript Practice from Book/chapter#01/index.ts
@@ -54,7 +54,7 @@ let withFirstName: WithFirstName = hasBoth;
  //
 
  type PoemWithPages = { name: string; pages: number; };
- type PoemWithRhymes = { name: string; rhymes: boolean; };
+ type PoemWithRhymes = { name: string; rhymes: boolean; }; 
  type Poem = PoemWithPages | PoemWithRhymes;
  const poem: Poem = Math.random() > 0.5 ? { name: "The Double Image", pages: 7 } : { name: "Her Kind", rhymes: true };
  poem.name; // Ok poem.pages; // ~~~~~ // Property 'pages' does not exist on type 'Poem'. // Property 'pages' does not exist on type 'PoemWithRhymes'. poem.rhymes; // ~~~~~~ // Property 'rhymes' does not exist on type 'Poem'. // Property 'rhymes' does not exist on type 'PoemWithPages'.

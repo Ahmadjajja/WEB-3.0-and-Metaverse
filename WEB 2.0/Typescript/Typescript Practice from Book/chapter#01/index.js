b9f65d89f8e7e2ea3046563da15664983530e3ba
Ahmad Jajja <86593662+Ahmadjajja@users.noreply.github.com>
2022-12-23 16:16:15 +0500

    chapter 4 typescript book reading

--- WEB 2.0/Typescript/Typescript Practice from Book/chapter#01/index.js
+++ WEB 2.0/Typescript/Typescript Practice from Book/chapter#01/index.js
@@ -15,3 +15,19 @@ const philosopher = 23;
 const f = null;
 const ok = { author: "Rita Dove", pages: 80, }; //ok
 const missing = { pages: 23 };
+const hasBoth = { firstName: "Lucille", lastName: "Clifton", };
+// Ok: `hasBoth` contains a `firstName` property of type `string` 
+let withFirstName = hasBoth;
+// Ok: `hasBoth` contains a `lastName` property of type `string` 
+let withLastName = hasBoth;
+console.log(withFirstName);
+console.log(withFirstName);
+const hasStartString = { start: new Date() }; // Error: Type 'string' is not assignable to type 'Date'. };
+console.log(hasStartString);
+// Ok: all fields match what's expected in Poet 
+const poetMatch = { born: 1928, name: "Maya Angelou" };
+const a = { activity: "walking", born: 1935, name: "Mary Oliver", };
+const extraProperty = a;
+console.log(extraProperty);
+const poem = Math.random() > 0.5 ? { name: "The Double Image", pages: 7 } : { name: "Her Kind", rhymes: true };
+poem.name; // Ok poem.pages; // ~~~~~ // Property 'pages' does not exist on type 'Poem'. // Property 'pages' does not exist on type 'PoemWithRhymes'. poem.rhymes; // ~~~~~~ // Property 'rhymes' does not exist on type 'Poem'. // Property 'rhymes' does not exist on type 'PoemWithPages'.
--- WEB 2.0/Typescript/Typescript Practice from Book/chapter#01/index.ts
+++ WEB 2.0/Typescript/Typescript Practice from Book/chapter#01/index.ts
@@ -23,3 +23,44 @@ const ok: Book = { author: "Rita Dove", pages: 80, };   //ok
 const missing: Book = { pages:23 };
 
 
+//Chapter #4 Started
+
+type WithFirstName = { firstName: string };
+type WithLastName = { lastName: string };
+const hasBoth = { firstName: "Lucille", lastName: "Clifton", };
+// Ok: `hasBoth` contains a `firstName` property of type `string` 
+let withFirstName: WithFirstName = hasBoth;
+ // Ok: `hasBoth` contains a `lastName` property of type `string` 
+ let withLastName: WithLastName = hasBoth;
+ console.log(withFirstName);
+ console.log(withFirstName);
+
+ type TimeRange = { start: Date; };
+ const hasStartString: TimeRange = { start: new Date()} // Error: Type 'string' is not assignable to type 'Date'. };
+ console.log(hasStartString);
+
+
+
+ type Poet = { born: number; name: string; }
+ // Ok: all fields match what's expected in Poet 
+ const poetMatch: Poet = { born: 1928, name: "Maya Angelou" };
+ const a = { activity: "walking", born: 1935, name: "Mary Oliver", };
+ const extraProperty: Poet = a
+ console.log(extraProperty);
+
+
+
+
+ //
+
+ type PoemWithPages = { name: string; pages: number; };
+ type PoemWithRhymes = { name: string; rhymes: boolean; };
+ type Poem = PoemWithPages | PoemWithRhymes;
+ const poem: Poem = Math.random() > 0.5 ? { name: "The Double Image", pages: 7 } : { name: "Her Kind", rhymes: true };
+ poem.name; // Ok poem.pages; // ~~~~~ // Property 'pages' does not exist on type 'Poem'. // Property 'pages' does not exist on type 'PoemWithRhymes'. poem.rhymes; // ~~~~~~ // Property 'rhymes' does not exist on type 'Poem'. // Property 'rhymes' does not exist on type 'PoemWithPages'.
+ 
+ 
+ 
+
+
+
